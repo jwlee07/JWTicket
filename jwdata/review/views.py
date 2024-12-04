84591bbb9302898d65ed45a252a67d07e4cd77ac
jwlee <apzk21413@gmail.com>
2024-12-05 00:46:13 +0900

    feat: 새창 예매 안내 닫기 완료

--- jwdata/review/views.py
+++ jwdata/review/views.py
@@ -41,19 +41,14 @@ def search_and_crawl(request):
                     EC.element_to_be_clickable((By.XPATH, '//*[@id="contents"]/div/div/div[1]/div[2]/a[1]/ul'))
                 ).click()
                 time.sleep(3)
-
-                # iframe 요소 모두 가져오기
-                WebDriverWait(driver, 10).until(
-                    EC.presence_of_all_elements_located((By.TAG_NAME, 'iframe'))
-                )
-                iframes = driver.find_elements(By.TAG_NAME, 'iframe')
-
-                # 각 iframe의 'name' 속성 출력
-                for iframe in iframes:
-                    iframe_name = iframe.get_attribute('name')
-                    print(f"iframe name: {iframe_name}")
+                
+                driver.switch_to.window(driver.window_handles[1])
+                time.sleep(1)
+                
+                popup_close_button = driver.find_element(By.XPATH, '//*[@id="popup-prdGuide"]/div/div[3]/button')
+                driver.execute_script("arguments[0].click();", popup_close_button)
                 time.sleep(3)
-
+                
             finally:
                 driver.quit()
 
