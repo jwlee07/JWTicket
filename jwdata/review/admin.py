5017afc62ee835f772f88251f0090ba42b005435
jwlee <apzk21413@gmail.com>
2024-12-25 15:29:29 +0900

    feat: admin Seat 추가

--- jwdata/review/admin.py
+++ jwdata/review/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Concert, Review
+from .models import Concert, Review, Seat
 
 # Register your models here.
 
 admin.site.register(Concert)
 admin.site.register(Review)
+admin.site.register(Seat)
